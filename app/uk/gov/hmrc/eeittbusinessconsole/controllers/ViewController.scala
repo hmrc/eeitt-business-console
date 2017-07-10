d5a47af8e3ada2805b157c1a268e6d95671d49ef
daniel.connelly <danile.connelly@digital.hmrc.gov.uk>
2017-07-10 10:58:47 +0100

    adding in basic structure to display page

--- app/uk/gov/hmrc/eeittbusinessconsole/controllers/ViewController.scala
+++ app/uk/gov/hmrc/eeittbusinessconsole/controllers/ViewController.scala
@@ -1,4 +1,4 @@
-@*
+/*
  * Copyright 2017 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,16 +12,21 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *@
+ */
 
-@(pageTitle: String, heading: String, message: String)(implicit request: Request[_], messages: Messages)
+package uk.gov.hmrc.eeittbusinessconsole.controllers
 
-@contentHeader = {
-  <h1>@heading</h1>
+import uk.gov.hmrc.play.frontend.controller.FrontendController
+import play.api.mvc._
+import scala.concurrent.Future
+import play.api.Play.current
+import play.api.i18n.Messages.Implicits._
+
+object ViewController extends ViewControllerHelper
+
+trait ViewControllerHelper extends FrontendController {
+
+  def loadPage = Action.async { implicit request =>
+    Future.successful(Ok(uk.gov.hmrc.eeittbusinessconsole.views.html.business_console()))
+  }
 }
-
-@mainContent = {
-  <p>@message</p>
-}
-
-@govuk_wrapper(appConfig = uk.gov.hmrc.eeittbusinessconsole.FrontendAppConfig, title = pageTitle, contentHeader = Some(contentHeader), mainContent = mainContent)
